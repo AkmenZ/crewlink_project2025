39851e00e26e30ef78f55f091068ea8f6152fd7c
AkmenZ <akmenz89@gmail.com>
2025-04-04 10:59:11 +0100

    feat: Auth screen, Package upgrade, ios pod upgrade to 13

---
+++ lib/providers/common_providers.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'common_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$createAccountHash() => r'7795a5b3bec3488bd1b1f9db3575943714418f1b';
+
+/// See also [CreateAccount].
+@ProviderFor(CreateAccount)
+final createAccountProvider =
+    AutoDisposeNotifierProvider<CreateAccount, bool>.internal(
+  CreateAccount.new,
+  name: r'createAccountProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$createAccountHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CreateAccount = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
